c99854e5076577845b489966ff257fedcb25ff78
StrangeSparrow <73070412+StrangeSparrow@users.noreply.github.com>
2021-01-18 14:05:22 +0300

    userService test

---
+++ src/test/java/org/myapp/taskmanager/service/UserServiceImplTest.java
@@ -0,0 +1,77 @@
+package org.myapp.taskmanager.service;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.myapp.taskmanager.dto.UserDto;
+import org.myapp.taskmanager.model.User;
+import org.myapp.taskmanager.repositories.UserRepositories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+class UserServiceImplTest {
+    private List<User> listTestData;
+    @MockBean
+    private UserRepositories userRepositories;
+    @Autowired
+    private UserServiceImpl service;
+
+    @BeforeEach
+    void setUp() {
+        listTestData = new ArrayList<>();
+        listTestData.add(new User(1, "test1", "test1", User.Role.admin, null, null, null, null));
+        listTestData.add(new User(2, "test2", "test2", User.Role.executor, null, null, null, null));
+        listTestData.add(new User(3, "test3", "test3", User.Role.executor, null, null, null, null));
+        listTestData.add(new User(4, "test4", "test4", User.Role.manager, null, null, null, null));
+    }
+
+    @Test
+    void getAll() {
+        Mockito.when(userRepositories.findAll()).thenReturn(listTestData);
+
+        List<UserDto> users = service.getAll();
+
+        assertEquals(4, users.size());
+        assertEquals("test2", users.get(1).getName());
+    }
+
+    @Test
+    void getById() {
+        given(userRepositories.findById(3)).willReturn(Optional.of(listTestData.get(2)));
+
+        UserDto user = service.getById(3);
+
+        assertEquals(3, user.getId());
+        assertEquals("test3", user.getName());
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteUserById() {
+    }
+
+    @Test
+    void getUsersByOwnerTaskId() {
+    }
+
+    @Test
+    void getUsersByExecutorTaskId() {
+    }
+}
